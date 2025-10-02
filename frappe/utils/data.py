2774e5cba7a5c94c385ae7a3521180858d156de4
kehwar <erickkwr@gmail.com>
2025-10-02 06:30:33 +0000

    feat: skip internal links

--- frappe/utils/data.py
+++ frappe/utils/data.py
@@ -1987,7 +1987,7 @@ def expand_relative_urls(html: str) -> str:
 	def _expand_relative_urls(match):
 		to_expand = list(match.groups())
 
-		if not to_expand[2].startswith(("mailto", "data:", "tel:")):
+		if not to_expand[2].startswith(("mailto", "data:", "tel:", "#")):
 			if not to_expand[2].startswith("/"):
 				to_expand[2] = "/" + to_expand[2]
 			to_expand.insert(2, url)
