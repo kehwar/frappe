51d2a0b963f55715ab5ada79616339aecdbfc218
kehwar <erickkwr@gmail.com>
2026-01-12 14:25:48 +0000

    fix: update status options in Scheduled Job Log to include 'Start'

--- frappe/core/doctype/scheduled_job_log/scheduled_job_log.py
+++ frappe/core/doctype/scheduled_job_log/scheduled_job_log.py
@@ -8,21 +8,23 @@ from frappe.query_builder.functions import Now
 
 
 class ScheduledJobLog(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
 
-	from typing import TYPE_CHECKING
+    from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
+    if TYPE_CHECKING:
+        from frappe.types import DF
 
-		debug_log: DF.Code | None
-		details: DF.Code | None
-		scheduled_job_type: DF.Link
-		status: DF.Literal["Scheduled", "Complete", "Failed"]
+        debug_log: DF.Code | None
+        details: DF.Code | None
+        scheduled_job_type: DF.Link
+        status: DF.Literal["Scheduled", "Start", "Complete", "Failed"]
 
-	# end: auto-generated types
-	@staticmethod
-	def clear_old_logs(days=90):
-		table = frappe.qb.DocType("Scheduled Job Log")
-		frappe.db.delete(table, filters=(table.modified < (Now() - Interval(days=days))))
+    # end: auto-generated types
+    @staticmethod
+    def clear_old_logs(days=90):
+        table = frappe.qb.DocType("Scheduled Job Log")
+        frappe.db.delete(
+            table, filters=(table.modified < (Now() - Interval(days=days)))
+        )
