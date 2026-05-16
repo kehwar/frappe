0d5fd757d7da3c25ff563ec1deff022a28544051
kehwar <erickkwr@gmail.com>
2026-05-16 21:40:09 +0000

    refactor: relax rules

--- frappe/custom/doctype/customize_form/customize_form.py
+++ frappe/custom/doctype/customize_form/customize_form.py
@@ -349,16 +349,6 @@ class CustomizeForm(Document):
 				)
 				return False
 
-		elif prop == "reqd" and (
-			(
-				frappe.db.get_value("DocField", {"parent": self.doc_type, "fieldname": df.fieldname}, "reqd")
-				== 1
-			)
-			and (df.get(prop) == 0)
-		):
-			frappe.msgprint(_("Row {0}: Not allowed to disable Mandatory for standard fields").format(df.idx))
-			return False
-
 		elif (
 			prop == "in_list_view"
 			and df.get(prop)
@@ -379,18 +369,6 @@ class CustomizeForm(Document):
 
 		elif prop == "unique":
 			self.flags.update_db = True
-
-		elif (
-			prop == "read_only"
-			and cint(df.get("read_only")) == 0
-			and frappe.db.get_value(
-				"DocField", {"parent": self.doc_type, "fieldname": df.fieldname}, "read_only"
-			)
-			== 1
-		):
-			# if docfield has read_only checked and user is trying to make it editable, don't allow it
-			frappe.msgprint(_("You cannot unset 'Read Only' for field {0}").format(df.label))
-			return False
 
 		elif prop == "options" and df.get("fieldtype") not in ALLOWED_OPTIONS_CHANGE:
 			frappe.msgprint(_("You can't set 'Options' for field {0}").format(df.label))
